b6c4f1fdab910a56794cbb503a689a11725eb418
nico-harison <nico.harison36@gmail.com>
2025-04-26 02:36:26 +0500

    ✨

--- src/screens/Account/AccountScreen.js
+++ src/screens/Account/AccountScreen.js
@@ -8,6 +8,7 @@ import {useDispatch, useSelector} from 'react-redux';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {clearState} from '../../redux/slices/userSlice';
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
 const ItemSetting = ({name, action, icon, iconSize, description}) => {
   return (
@@ -57,9 +58,8 @@ export default function AccountScreen() {
         {
           text: 'Delete',
           onPress: async () => {
+            await deleteUserAccount();
             dispatch(clearState());
-            const userCredential = await auth().currentUser.delete();
-            console.log({userCredential});
             navigation.replace('Login');
           },
           style: 'destructive',
@@ -67,6 +67,61 @@ export default function AccountScreen() {
       ],
       {cancelable: true},
     );
+  };
+
+  const deleteUserAccount = async () => {
+    try {
+      if (user && user.email === user?.email) {
+        const userDoc = await firestore()
+          .collection('users')
+          .where('email', '==', user.email)
+          .get();
+
+        if (!userDoc.empty) {
+          userDoc.forEach(async doc => {
+            await firestore().collection('users').doc(doc.id).delete();
+          });
+        } else {
+          console.log('No matching user document found in Firestore');
+        }
+
+        const checkInsDocs = await firestore()
+          .collection('checkIns')
+          .where('uid', '==', user.uid)
+          .get();
+
+        if (!checkInsDocs.empty) {
+          checkInsDocs.forEach(async doc => {
+            await firestore().collection('checkIns').doc(doc.id).delete();
+          });
+        } else {
+          console.log('No matching checkIns document found in Firestore');
+        }
+
+        const listenersDocs = await firestore()
+          .collection('Listeners')
+          .where('uid', '==', user.uid)
+          .get();
+
+        if (!listenersDocs.empty) {
+          listenersDocs.forEach(async doc => {
+            await firestore().collection('Listeners').doc(doc.id).delete();
+          });
+        } else {
+          console.log('No matching Listeners document found in Firestore');
+        }
+
+        const {currentUser} = auth();
+
+        await currentUser.delete();
+      } else {
+        console.log(
+          'No user is currently signed in or the emails do not match',
+        );
+      }
+    } catch (error) {
+      console.error('Error deleting user account:', error);
+    }
   };
 
   return (
